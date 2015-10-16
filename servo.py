8aefa263a83f869710ff17d1a536b040abbabc87
Amira <amira.dhaile@gmail.com>
2015-10-16 14:54:56 -0700

    Changed range values.

--- servo.py
+++ servo.py
@@ -5,15 +5,21 @@ import time
 
 pwm = PWM(0x40)
 
-first = 150
+first  = 150
 second = 300
-third = 600
+third  = 600
 
-pwm.setPWMFreq(60)
+pwm.setPWMFreq(40)
+
 while (True):
     pwm.setPWM(0, 0, first)
+    pwm.setPWM(4, 0, first)
     time.sleep(1)
+
     pwm.setPWM(0, 0, second)
+    pwm.setPWM(4, 0, second)
     time.sleep(1)
+
     pwm.setPWM(0, 0, third)
+    pwm.setPWM(4, 0, third)
     time.sleep(1)
