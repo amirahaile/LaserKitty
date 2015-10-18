b3b8ec9864010e9138685fbc535a17da08b35dcd
amirahaile <amira.dhaile@gmail.com>
2015-10-18 10:38:43 -0700

    Added GUI page.

--- kittylaserbot/config/routes.rb
+++ kittylaserbot/config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
 
   root 'users#index'
 
-  resources :users, only: [ :new, :create, :destroy ]
+  resources :users, only: [ :new, :create, :show, :destroy ]
   resources :sessions, only: [ :create, :destroy ]
 
   get 'auth/:provider', to: 'session#new', as: 'auth'
   get 'auth/:provider/callback', to: 'session#create', as: 'callback'
+  get 'bot_gui', to: 'bot_guis#show', as: 'bot_gui'
 end
