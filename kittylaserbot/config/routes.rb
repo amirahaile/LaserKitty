dbdac365004cf2e6487ed8de06c22f1bee6f4d5e
amirahaile <amira.dhaile@gmail.com>
2015-10-18 13:27:43 -0700

    Added route for the GUI show view

--- kittylaserbot/config/routes.rb
+++ kittylaserbot/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [ :new, :create, :show, :destroy ]
   resources :sessions, only: [ :create, :destroy ]
 
-  get 'auth/:provider', to: 'session#new', as: 'auth'
+  get 'auth/:provider',          to: 'session#new',    as: 'auth'
   get 'auth/:provider/callback', to: 'session#create', as: 'callback'
-  get 'bot_gui', to: 'bot_guis#show', as: 'bot_gui'
+  get 'bot_gui',                 to: 'bot_guis#show',  as: 'bot_gui'
 end
