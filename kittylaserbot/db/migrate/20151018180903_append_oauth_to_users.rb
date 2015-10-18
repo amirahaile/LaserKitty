73f4ebe9ab6265e65117d4c508734606a5d19dbf
amirahaile <amira.dhaile@gmail.com>
2015-10-18 11:12:21 -0700

    Added oauth rows.

---
+++ kittylaserbot/db/migrate/20151018180903_append_oauth_to_users.rb
@@ -0,0 +1,8 @@
+class AppendOauthToUsers < ActiveRecord::Migration
+  def change
+    change_table :users do |t|
+      t.string "uid"
+      t.string "provider"
+    end
+  end
+end
--- kittylaserbot/db/schema.rb
+++ kittylaserbot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151018032058) do
+ActiveRecord::Schema.define(version: 20151018180903) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",      null: false
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20151018032058) do
     t.string   "association"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "uid"
+    t.string   "provider"
   end
 
 end
