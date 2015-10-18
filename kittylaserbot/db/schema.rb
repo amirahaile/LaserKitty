e14a0a842aaaee4328f4b470341919cfb54a99e7
amirahaile <amira.dhaile@gmail.com>
2015-10-18 13:27:16 -0700

    Added username row to users

--- kittylaserbot/db/schema.rb
+++ kittylaserbot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151018180903) do
+ActiveRecord::Schema.define(version: 20151018202442) do
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",      null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20151018180903) do
     t.datetime "updated_at",      null: false
     t.string   "uid"
     t.string   "provider"
+    t.string   "username"
   end
 
 end
