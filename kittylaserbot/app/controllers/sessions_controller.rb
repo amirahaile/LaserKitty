0699de28dd18bb7baea104740dfce420dd3bf04c
amirahaile <amira.dhaile@gmail.com>
2015-10-18 10:30:08 -0700

    Created #create and #destroy.

--- kittylaserbot/app/controllers/sessions_controller.rb
+++ kittylaserbot/app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+
+  def create # sign in
+    @user = User.find_by(email: params[:session][:email])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:error] = "Incorrect username or password"
+      # TODO: @user.errors.messages
+      redirect_to root_path
+    end
+  end
+
+  def destroy # sign out
+    reset_session
+    redirect_to root_path
+  end
 end
