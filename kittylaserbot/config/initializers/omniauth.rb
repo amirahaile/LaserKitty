139ed52c9d071a0a360466bea88185513dfbac2d
amirahaile <amira.dhaile@gmail.com>
2015-10-18 17:32:51 -0700

    Added Github.

--- kittylaserbot/config/initializers/omniauth.rb
+++ kittylaserbot/config/initializers/omniauth.rb
@@ -1,4 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :instagram, ENV['INSTAGRAM_CLIENT_ID'], ENV['INSTAGRAM_CLIENT_SECRET']
+  provider :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET']
 end
