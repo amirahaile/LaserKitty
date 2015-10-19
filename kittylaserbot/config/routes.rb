68fd96995f3fdb36ab21aee8548660109c73d9d0
amirahaile <amira.dhaile@gmail.com>
2015-10-19 13:16:51 -0700

    Removed unnecessary route.

--- kittylaserbot/config/routes.rb
+++ kittylaserbot/config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   root 'users#index'
 
   resources :users, only: [ :new, :create, :show, :destroy ]
-  resources :sessions, only: [ :create, :destroy ]
 
-  get 'auth/:provider',          to: 'session#new',    as: 'auth'
-  get 'auth/:provider/callback', to: 'session#create', as: 'callback'
+  get 'auth/:provider',          to: 'sessions#new',   as: 'auth'
+  get 'auth/:provider/callback', to: 'sessions#create' 
   get 'bot_gui',                 to: 'bot_guis#show',  as: 'bot_gui'
 end
