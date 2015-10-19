6c78d1be92d618da2cbe4b91bdcfad74fef6bb14
amirahaile <amira.dhaile@gmail.com>
2015-10-19 14:04:07 -0700

    Added FB credentials and removed Instagram's.

--- kittylaserbot/config/initializers/omniauth.rb
+++ kittylaserbot/config/initializers/omniauth.rb
@@ -3,4 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :instagram, ENV['INSTAGRAM_ID'], ENV['INSTAGRAM_SECRET']
   provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET']
   provider :twitter, ENV['TWITTER_ID'], ENV['TWITTER_SECRET']
+  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
 end
