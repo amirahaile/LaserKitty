7d0fa52b6c4b87f0c3fb08b14454a09742c29b4a
amirahaile <amira.dhaile@gmail.com>
2015-10-19 13:18:34 -0700

    Consolidated into one method for omniauth.

--- kittylaserbot/app/models/user.rb
+++ kittylaserbot/app/models/user.rb
@@ -5,37 +5,19 @@ class User < ActiveRecord::Base
 
 # Methods ----------------------------------------------------------------------
   def self.find_or_create_from_omniauth(auth_hash)
-    instagram = auth_hash["user"]
-    auth_uid = instagram.nil? ? "nil" : instagram["id"]
-    auth_provider = instagram.nil? ? "developer" : "instagram"
+    auth_uid = auth_hash["uid"]
+    auth_provider = auth_hash["provider"]
 
-    user = User.where(
-      uid: auth_uid,
+    user = User.where(uid: auth_uid,
       provider: auth_provider).first_or_initialize
 
-    user.uid      = instagram.nil? ? "nil" : instagram["id"]
-    user.provider = instagram.nil? ? "developer" : "instagram"
-    user.username = instagram.nil? ? auth_hash["info"]["name"] : instagram["username"]
-
-    return user.save ? user : nil
-  end
-
-  def self.find_or_create_from_github(auth_hash)
-    auth_uid = auth_hash["id"].to_s
-    auth_provider = "github"
-
-    # .first_or_initialize not working…
-    user = User.where(uid: auth_uid, provider: auth_provider).first_or_initialize
-
-    user.uid = auth_uid
-    user.provider = auth_provider
-    user.username = auth_hash["username"]
-    user.first_name = auth_hash["first_name"]
-    user.last_name = auth_hash["last_name"]
+    user.username = auth_hash["info"]["nickname"]
+    user.first_name = auth_hash["info"]["first_name"]
+    user.last_name = auth_hash["info"]["last_name"]
     user.email = auth_hash["email"]
-    # bcrypt has it's own validations; temp passwords
-    user.password = 'github456!!'
-    user.password_confirmation = 'github456!!'
+    # temporary passwords to pass bcrypt's validation
+    user.password = "aoijfeoveoinwe3983984upjf393hf83nf"
+    user.password_confirmation = "aoijfeoveoinwe3983984upjf393hf83nf"
 
     return user.save ? user : nil
   end
