e8125ff6129aae48457f12b86a059865a788c728
amirahaile <amira.dhaile@gmail.com>
2015-10-19 13:50:40 -0700

    Added twitter's credentials.

--- kittylaserbot/config/initializers/omniauth.rb
+++ kittylaserbot/config/initializers/omniauth.rb
@@ -2,4 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :instagram, ENV['INSTAGRAM_ID'], ENV['INSTAGRAM_SECRET']
   provider :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET']
+  provider :twitter, ENV['TWITTER_ID'], ENV['TWITTER_SECRET']
 end
