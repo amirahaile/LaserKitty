4acb66c143cbddcd4212eb468613644d212d5652
amirahaile <amira.dhaile@gmail.com>
2015-10-22 14:04:01 -0700

    Created email templates and #welcome_email(user).

--- kittylaserbot/app/mailers/user_mailer.rb
+++ kittylaserbot/app/mailers/user_mailer.rb
@@ -1,2 +1,8 @@
 class UserMailer < ApplicationMailer
+  default from: 'register@laserkittybot.com'
+
+  def welcome_email(user)
+    @user = user
+    mail(to: @user.email, subject: 'Your invite to kittylaserbot.com has been requested.')
+  end
 end
