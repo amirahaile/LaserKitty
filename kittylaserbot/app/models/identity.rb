0c3db8d441c36482e437469b32bbcf2c0827161b
amirahaile <amira.dhaile@gmail.com>
2015-10-22 16:56:27 -0700

    Association to user and methods for omniauth.

--- kittylaserbot/app/models/identity.rb
+++ kittylaserbot/app/models/identity.rb
@@ -1,2 +1,14 @@
 class Identity < ActiveRecord::Base
+# Associations -----------------------------------------------------------------
+  belongs_to :user
+
+# Methods ----------------------------------------------------------------------
+  def self.find_with_omniauth(auth_hash)
+    # returns Identity obj or `nil`
+    find_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
+  end
+
+  def self.create_with_omniauth(auth_hash)
+    create(uid: auth_hash['uid'], provider: auth_hash['provider'])
+  end
 end
