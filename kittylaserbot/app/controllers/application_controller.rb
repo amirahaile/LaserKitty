60f4b34e861be201343e78e2dd7a6867311ed9cb
amirahaile <amira.dhaile@gmail.com>
2015-10-23 11:26:21 -0700

    Finally got sign in with identities and user objs right.

--- kittylaserbot/app/controllers/application_controller.rb
+++ kittylaserbot/app/controllers/application_controller.rb
@@ -10,4 +10,19 @@ class ApplicationController < ActionController::Base
       redirect_to root_path
     end
   end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  helper_method :signed_in?, :current_user
+
+  def signed_in?
+    !!current_user
+  end
+
+  def current_user=(user)
+    @current_user = user
+    session[:user_id] = user.nil? ? nil : user.id
+  end
 end
--- kittylaserbot/app/controllers/sessions_controller.rb
+++ kittylaserbot/app/controllers/sessions_controller.rb
@@ -1,36 +1,66 @@
 class SessionsController < ApplicationController
-  def create # sign in
-    error = params["error"] ? true : false # github error handling
+  #skip_before_filter :verify_authenticity_token, only: :create
 
-    if error
-      @error_code = "400"
-      @error_type = params["error"]
-      @error_msg  = params["error_description"]
-      render 'errors/show'
-      return
+  # This logic allows users to link multiple OAuth identities to their account *while* they're signed in. Doesn't allow the user to link them together if they're signed out. Not a perfect solution, but does help by not creating a new user with every OAuth identity.
+
+  # NOTE: Maybe check for matching emails to link new OAuth identities with a user.
+
+  def create # sign in
+    auth = auth_hash # private Session method
+    provider = params[:provider].capitalize
+
+    # find identity
+    @identity = Identity.find_with_omniauth(auth)
+
+    if @identity.nil?
+      # no identity was found; create one
+      @identity = Identity.create_with_omniauth(auth)
     end
 
-    # oauth
-    @user = User.find_or_create_from_omniauth(auth_hash)
+    if signed_in?
+      if @identity.user == current_user
+        # signed in user tried to link an already linked account
+        msg = "#{provider} account is already linked."
+      else
+        # delete associated user if it doesn't have any other identities
+        # result of same person creating multiple users
+        former_user = @identity.user
+        if !former_user.nil? && former_user.identities.length <= 1
+          former_user.destroy
+        end
 
-    # manual
-    # if !params[:session].nil?
-    #   @user = User.find_by(email: params[:session][:email])
-    #
-    #   unless @user && @user.authenticate(params[:session][:password])
-    #     flash[:error] = "Incorrect username or password"
-    #     # TODO: @user.errors.messages
-    #     redirect_to root_path
-    #   end
-    # end
+        # associate the new identity with signed in user
+        @identity.user = current_user
+        @identity.save()
+        msg = "Successfully linked #{provider} account."
+      end
+    else
+      if @identity.user.present?
+        # identity has an associated user
+        self.current_user = @identity.user # signing into session
+        msg = ""
+      else
+        # no user associated; create a new one
+        # TODO: new up a User object so it can prepopulate with info grabbed from omniauth
+        msg = "#{provider} account successfully linked! Please finish registering."
+        redirect_to new_user_path, notice: msg
+      end
+    end
 
-    session[:user_id] = @user.id
-
-    redirect_to bot_gui_path
+    case current_user.invite_status
+    when "accepted"
+      redirect_to bot_gui_path, notice: msg
+    when "pending"
+      render :request_pending
+    when "rejected"
+      render :request_rejected
+    else
+      render :request_pending
+    end
   end
 
   def destroy # sign out
-    reset_session
+    self.current_user = nil
     redirect_to root_path
   end
 
