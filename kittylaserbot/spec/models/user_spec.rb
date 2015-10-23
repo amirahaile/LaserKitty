2f3a8e98d2d386c7cb37da259d831d9d99b2dcf1
amirahaile <amira.dhaile@gmail.com>
2015-10-23 10:55:31 -0700

    Tests for User class method.

--- kittylaserbot/spec/models/user_spec.rb
+++ kittylaserbot/spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "class methods" do
+    describe "#create_from_omniauth(auth_hash)" do
+      let(:auth_hash) {
+        {
+          "provider" => "github",
+          "uid" => "93824844",
+          "info" => {
+            "email" => "jane@thevirgin.com",
+            "nickname" => "The Virgin",
+            "first_name" => "Jane",
+            "last_name" => "Villanueva"
+          }
+        }
+      }
+      let(:new_user) { User.create_from_omniauth(auth_hash) }
+
+      it "creates a new instance of User" do
+        expect(new_user).to be_an_instance_of User
+        expect(new_user.id).to eq 1
+      end
+
+      it "isn't associated with any Identity objects" do
+        expect(new_user.identities).to eq []
+      end
+    end
+  end
 end
