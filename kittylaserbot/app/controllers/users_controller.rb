e078677019e62bbe1eec1a9909d92c914e6842b0
amirahaile <amira.dhaile@gmail.com>
2015-10-23 13:14:41 -0700

    Created #new and #update.

--- kittylaserbot/app/controllers/users_controller.rb
+++ kittylaserbot/app/controllers/users_controller.rb
@@ -1,2 +1,25 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.find(session[:user_id])
+  end
+
+  def update
+    user_id = session[:user_id]
+    User.update(user_id, user_params)
+    user = User.find(user_id)
+
+    if user.invite_status == "pending"
+      render "sessions/request_pending"
+    elsif user.invite_status == "rejected"
+      render "sessions/request_rejected"
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
