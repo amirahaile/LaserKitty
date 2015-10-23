3da46797767968c752ebd89bad2ab3e4057c68f7
amirahaile <amira.dhaile@gmail.com>
2015-10-23 13:14:21 -0700

    Removed  from class methods.

--- kittylaserbot/app/models/identity.rb
+++ kittylaserbot/app/models/identity.rb
@@ -9,10 +9,10 @@ class Identity < ActiveRecord::Base
 # Methods ----------------------------------------------------------------------
   def self.find_with_omniauth(auth_hash)
     # returns Identity obj or `nil`
-    Identity.find_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
+    find_by(uid: auth_hash['uid'], provider: auth_hash['provider'])
   end
 
   def self.create_with_omniauth(auth_hash)
-    Identity.create(uid: auth_hash['uid'], provider: auth_hash['provider'])
+    create(uid: auth_hash['uid'], provider: auth_hash['provider'])
   end
 end
