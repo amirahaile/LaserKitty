8ef88a41babc6347c2f2f6f64f5ebf342c8783e5
amirahaile <amira.dhaile@gmail.com>
2015-10-25 13:35:53 -0700

    Added logout route.

--- kittylaserbot/config/routes.rb
+++ kittylaserbot/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   get '/auth/:provider',          to: 'sessions#new',   as: 'auth'
   get '/auth/:provider/callback', to: 'sessions#create'
   # sessions
-  get '/logout',                  to: 'sessions#destroy'
+  get '/logout',                  to: 'sessions#destroy', as: 'logout'
   # GUI
   get '/bot_gui',                 to: 'bot_guis#show',  as: 'bot_gui'
 
