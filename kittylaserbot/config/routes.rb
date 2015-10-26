b09a425c0288c53e81ac996529accc9282f9b81f
amirahaile <amira.dhaile@gmail.com>
2015-10-26 16:01:11 -0700

    Added route for changing bot's attributes.

--- kittylaserbot/config/routes.rb
+++ kittylaserbot/config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   get '/logout',                  to: 'sessions#destroy', as: 'logout'
   # GUI
   get '/bot_gui',                 to: 'bot_guis#show',  as: 'bot_gui'
+  get '/power_bot',               to: 'bot_guis#io'
 
   # RaspPi API
   get '/raspi', to: 'bot_guis#prepare'
