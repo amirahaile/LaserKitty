23a78ff640436c661c4758aa07e654cab8183afb
amirahaile <amira.dhaile@gmail.com>
2015-10-26 15:28:52 -0700

    Disable start/stop button when Bot is 'on.'

--- kittylaserbot/app/controllers/bot_guis_controller.rb
+++ kittylaserbot/app/controllers/bot_guis_controller.rb
@@ -1,6 +1,11 @@
 class BotGuisController < ApplicationController
   before_action :require_login
 
+  def show
+    bot = Bot.find(session[:bot_id])
+    @io_status = bot.io
+  end
+
   def prepare
     bot = Bot.find(session[:bot_id])
     status = bot.io
