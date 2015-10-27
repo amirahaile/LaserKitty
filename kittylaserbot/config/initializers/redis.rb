a2a8bfa5d201e620b8a0b5469edbf35539ea13dc
amirahaile <amira.dhaile@gmail.com>
2015-10-27 15:55:34 -0700

    Installed Redis.

---
+++ kittylaserbot/config/initializers/redis.rb
@@ -0,0 +1,7 @@
+if Rails.env.test?
+  $redis = MockRedis.new
+elsif Rails.env.development?
+  $redis = Redis.new(:host => 'localhost', :port => 6379)
+elsif Rails.env.production?
+  $redis = Redis.new(url: ENV["REDIS_URL"])
+end
