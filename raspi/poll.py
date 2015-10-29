9c16b9364074f129e91620025d5890b17f9cdcdc
Amira <amira.dhaile@gmail.com>
2015-10-29 12:54:36 -0700

    Added a post request.

--- raspi/poll.py
+++ raspi/poll.py
@@ -18,16 +18,20 @@ def killProcesses():
     
     # kills parent process
     ustream_process.terminate()
+    bot_play_process.terminate()
     # kills child processes
     os.killpg(ustream_process.pid, signal.SIGKILL)
+    os.killpg(bot_play_process.pid, signal.SIGKILL)
     print("Killed it.")
     print("Is it dead?: %d" % ustream_process.poll())
 
 def startProcesses():
-    global ustream_process, ustream_starttime
+    global ustream_process, ustream_starttime, bot_play_process
+    ustream_path = "/home/pi/ustream"
+    browser_play_path = "/home/pi/dev/LaserKitty/browser_play.py"
     
-    ustream_process = Popen(["/home/pi/ustream"], start_new_session=True)
-    # bot script 
+    ustream_process = Popen([ustream_path], start_new_session=True)
+    bot_play_process = Popen([browser_play_path], start_new_session=True)
     ustream_starttime = datetime.datetime.now()
     print("Now streaming!")
     print(ustream_starttime)
@@ -71,15 +75,18 @@ while True:
     #--- COMMANDS
             
     if process_running and requested_status == "on":
-        pass
         # do nothing; unless it's been too long
-        #current_time = datetime.datetime.now()
-        #time_diff = current_time - ustream_starttime
-        #time_diff = time_diff.total_seconds()
-        #print("Time difference (secs): %d" % time_diff)
+        current_time = datetime.datetime.now()
+        time_diff = current_time - ustream_starttime
+        time_diff = time_diff.total_seconds()
+        print("Time difference (secs): %d" % time_diff)
 
-        #if time_diff >= 300: # 5 min
-         #   killProcesses()
+        if time_diff >= 300: # 5 min
+            killProcesses()
+
+            # update browser status
+            data = { 'io': 'off', 'commander': 'pi' }
+            requests.post('http://kittylaserbot.com/update_bot', json=data)
     
     elif not process_running and requested_status == "on":
         startProcesses()
