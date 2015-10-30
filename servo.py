b1ed91625d7601d7c37b987b43e5a64a5b10a03a
Amira <amira.dhaile@gmail.com>
2015-10-29 17:47:10 -0700

    Figuring out servo math.

--- servo.py
+++ servo.py
@@ -1,25 +1,61 @@
 #!/user/bin/python
 
 from Adafruit_PWM_Servo_Driver import PWM
+import RPi.GPIO as GPIO
 import time
 
+# METHODS
+freq = 50
+min_deg = 0
+max_deg = 45 # servo max = 150
+time_per_tick = (1.0 / freq) / 4096 # servo max tick = 4096
+
+def calcTick(angle):
+    milisec = (angle / 100.0) + 0.6
+    microsec = milisec / 1000
+    tick = microsec / time_per_tick
+    return tick
+    
+
+
+print(calcTick(30))
+print(calcTick(90))
+print(calcTick(120))
+
+
+
+
+
 pwm = PWM(0x40)
+pwm.setPWMFreq(freq)
 
-first  = 150
-second = 300
-third  = 600
+#print("longest")
+#time.sleep(1)
+##pwm.setPWM(4, 0, 245)
+##time.sleep(3)
+##pwm.setPWM(4, 0, 183)
+##time.sleep(3)
+##pwm.setPWM(4, 0, 500)
 
-pwm.setPWMFreq(40)
+##print("half")
+##time.sleep(5)
+##pwm.setPWM(4, 0, 245)
+##
+##print("littlest")
+##time.sleep(5)
+##pwm.setPWM(4, 0, 183)
 
-while (True):
-    pwm.setPWM(0, 0, first)
-    pwm.setPWM(4, 0, first)
-    time.sleep(1)
 
-    pwm.setPWM(0, 0, second)
-    pwm.setPWM(4, 0, second)
-    time.sleep(1)
 
-    pwm.setPWM(0, 0, third)
-    pwm.setPWM(4, 0, third)
-    time.sleep(1)
+#while (True):
+    #pwm.setPWM(0, 0, first)
+    #pwm.setPWM(4, 0, first)
+    #time.sleep(1)
+
+    #pwm.setPWM(0, 0, second)
+    #pwm.setPWM(4, 0, second)
+    #time.sleep(1)
+
+    #pwm.setPWM(0, 0, third)
+    #pwm.setPWM(4, 0, third)
+    #time.sleep(1)
