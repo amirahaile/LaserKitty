117521f48d3543dbd8d3f8b8c24cfc857ba4981d
amirahaile <amira.dhaile@gmail.com>
2015-12-22 17:46:38 -0800

    Organized with whitespace and comments.

--- LaserBot/app.js
+++ LaserBot/app.js
@@ -5,10 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// routes variables
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+// **** //
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +25,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes set-up
 app.use('/', routes);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
