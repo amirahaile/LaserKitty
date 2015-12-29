a3c0dd88d9731de49eb25ffae0fbf44e1451edc4
amirahaile <amira.dhaile@gmail.com>
2015-12-29 13:36:44 -0800

    Separated out more route-specific tests.

--- LaserBot/test/controllers/gui_controller_tests.js
+++ LaserBot/test/controllers/gui_controller_tests.js
@@ -6,26 +6,8 @@ var request = require('supertest'),
 describe('GUI Controller', function(){
   var app = express();
 
-  describe('GET /gui', function(){
-    it('responds successfully with a 200 status', function(done){
-      app.get('/gui').expect(200, done);
-    });
-
-    // it('renders the GUI show view', function(done){
-    // });
-  });
-
-  // this request is only ever made by the RaspPi
-  // a view is unnecessary
   describe('GET /bot_status', function(){
     var getBotStatus = request(app).get('/bot_status').set('Accept', 'application/json');
-
-    it('responds with JSON', function(done){
-      getBotStatus
-        .expect('Content-Type', 'application/json')
-        .expect('Content-Length', '2')
-        .expect(200, done);
-    });
 
     it('returns the bot\'s status and controller', function(done){
       getBotStatus.expect(200, function(error, result){
@@ -35,15 +17,8 @@ describe('GUI Controller', function(){
     });
   });
 
-  // this request is made by either the app's online user or the RaspPi
   describe('POST /bot_status', function(){
     var postBotStatus = request(app).post('/bot_status').set('Content-Type', 'application/json');
-
-    it('accepts JSON', function(done){
-      postBotStatus
-        .send('{"power": "off", "controller": "manual"}')
-        .end(function(error, result){});
-    });
 
     it('updates the bot\'s status according to the received JSON', function(done){
       var oldBotStatus = bot.report();
@@ -59,8 +34,5 @@ describe('GUI Controller', function(){
       assert.notEqual(newBotStatus.controller, oldBotStatus.controller);
       assert.equal(newBotStatus.controller, 'manual');
     });
-
-    // it('renders the show view', function(done){
-    // });
   });
 });
---
+++ LaserBot/test/routes/gui_routes_tests.js
@@ -0,0 +1,53 @@
+var request = require('supertest'),
+    express = require('express'),
+    bot     = require('../../modules/bot'),
+    routes  = require('../../routes'),
+    assert  = require('assert');
+
+describe('GUI Routes', function(){
+  var app = express();
+
+  describe('GET /gui', function(){
+    it('responds successfully with a 200 status', function(done){
+      app.get('/gui').expect(200, done);
+    });
+
+    // it('renders the GUI show view', function(done){
+    // });
+  });
+
+  // this request is only ever made by the RaspPi
+  // a view is unnecessary
+  describe('GET /bot_status', function(){
+    var getBotStatus = request(app).get('/bot_status').set('Accept', 'application/json');
+
+    it('responds successfully with a 200 status', function(done){
+      getBotStatus.expect(200, done);
+    });
+
+    it('responds with JSON', function(done){
+      getBotStatus
+        .expect('Content-Type', 'application/json')
+        .expect('Content-Length', '2')
+        .expect(200, done);
+    });
+  });
+
+  // this request is made by either the app's online user or the RaspPi
+  describe('POST /bot_status', function(){
+    var postBotStatus = request(app).post('/bot_status').set('Content-Type', 'application/json');
+
+    it('responds successfully with a 200 status', function(done){
+      postBotStatus.expect(200, done);
+    });
+
+    it('accepts JSON', function(done){
+      postBotStatus
+        .send('{"power": "off", "controller": "manual"}')
+        .end(function(error, result){});
+    });
+
+    // it('renders the show view', function(done){
+    // });
+  });
+});
